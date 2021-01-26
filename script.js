773afab1ffab9f942677b6b95b17e86508d276dd
SS Jones <SSJones.code@gmail.com>
2021-01-26 14:57:07 -0700

    I created a for in loop to loop through an object and print the onject properties to the console.

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 //Conditional Statements: If-Else
-function getGrade(score) {
-    let grade;
-    // Write your code here
-    if (score <= 30 && score > 25) {
-        let grade = "A";
-        return grade;
-    } else if (score <= 25 && score > 20) {
-        let grade = "B"
-        return grade;
-    } else if (score <= 20 && score > 15) {
-        let grade = "C"
-        return grade;
-    } else if (score <= 15 && score > 10) {
-        let grade = "D"
-        return grade;
-    } else if (score <= 10 && score > 5) {
-        let grade = "E"
-        return grade;
-    } else {
-        let grade = "F";
-        return grade;
-    }
-}
+// function getGrade(score) {
+//     let grade;
+//     // Write your code here
+//     if (score <= 30 && score > 25) {
+//         let grade = "A";
+//         return grade;
+//     } else if (score <= 25 && score > 20) {
+//         let grade = "B"
+//         return grade;
+//     } else if (score <= 20 && score > 15) {
+//         let grade = "C"
+//         return grade;
+//     } else if (score <= 15 && score > 10) {
+//         let grade = "D"
+//         return grade;
+//     } else if (score <= 10 && score > 5) {
+//         let grade = "E"
+//         return grade;
+//     } else {
+//         let grade = "F";
+//         return grade;
+//     }
+// }
 
-function getGrade(score);
+// function getGrade(score)
+
+
+var actress = {
+    firstName: "Julia",
+    lastName: "Roberts",
+    dateOfBirth: "October 28, 1967",
+    nationality: "American",
+    firstMovie: "Satisfaction"
+};
+
+for (var property in actress) {
+    console.log("actress." + property + " = " + actress[property]);
+}
