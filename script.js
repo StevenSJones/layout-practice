3cb7d269c70fde61942da758adb09d883bdfacec
SS Jones <SSJones.code@gmail.com>
2021-01-25 19:07:56 -0700

    I successfully created an if else conditional statement that takes in a score of up to thirty and assigns a letter grade for certain scores.

---
+++ script.js
@@ -0,0 +1,26 @@
+//Conditional Statements: If-Else
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if (score <= 30 && score > 25) {
+        let grade = "A";
+        return grade;
+    } else if (score <= 25 && score > 20) {
+        let grade = "B"
+        return grade;
+    } else if (score <= 20 && score > 15) {
+        let grade = "C"
+        return grade;
+    } else if (score <= 15 && score > 10) {
+        let grade = "D"
+        return grade;
+    } else if (score <= 10 && score > 5) {
+        let grade = "E"
+        return grade;
+    } else {
+        let grade = "F";
+        return grade;
+    }
+}
+
+function getGrade(score);
